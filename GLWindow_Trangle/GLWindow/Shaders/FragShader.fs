7f2ab38d014b6aae8c8dc22a2f7fff2d1de08a41
GhostZwq <13913968214.com>
2018-08-29 23:20:40 +0800

    -- Texture Practice(Haven't done)

--- GLWindow_Trangle/GLWindow/Shaders/FragShader.fs
+++ GLWindow_Trangle/GLWindow/Shaders/FragShader.fs
@@ -1,8 +1,13 @@
-#version 330 core                           
-out vec4 FragColor;                         
+#version 330 core             
+              
+out vec4 FragColor;  
+                       
 in vec3 ourColor;        
-in vec3 ourPos;                   
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
 void main()                                 
 {                                           
-    FragColor = vec4(ourPos, 1.0);        
+    FragColor = vec4(1.0, 0.3, 0.2, 1.0);        
 }
