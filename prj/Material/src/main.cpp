a949ca3e9486bf98653eb2b4e1a6a7f1c1164a87
GhostZwq <13913968214.com>
2018-10-22 22:29:38 +0800

    -- Realize Cyan Plastic in real world

--- prj/Material/src/main.cpp
+++ prj/Material/src/main.cpp
@@ -210,12 +210,12 @@ int main()
 	//ourShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
 	//ourShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 	ourShader.setVec3("lightPos", lightPos);
-	ourShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-	ourShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-	ourShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+	ourShader.setVec3("material.ambient", 0.0f, 0.1f, 0.06f);
+	ourShader.setVec3("material.diffuse", 0.0f, 0.50980392f, 0.50980392f);
+	ourShader.setVec3("material.specular", 0.50196078f, 0.50196078f, 0.50196078f);
 	ourShader.setFloat("material.shininess", 32.0f);
-	ourShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-	ourShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+	ourShader.setVec3("light.ambient", 1.0f, 1.0f, 1.0f);
+	ourShader.setVec3("light.diffuse", 1.0f, 1.0f, 1.0f);
 	ourShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
 
@@ -266,18 +266,18 @@ int main()
 		ourShader.setMat4("view", view);
 		ourShader.setVec3("lightPos", lightPos);
 
-		glm::vec3 lightColor;
-		lightColor.x = sin(glfwGetTime() * 2.0f);
-		lightColor.y = sin(glfwGetTime() * 0.7f);
-		lightColor.z = sin(glfwGetTime() * 1.3f);
-		
-		glm::vec3 diffuseColor = lightColor     * glm::vec3(0.5f);
-		glm::vec3 ambientColor = diffuseColor   * glm::vec3(0.2f);
-		glm::vec3 specularColor = lightColor    * glm::vec3(0.5f);
+		//glm::vec3 lightColor;
+		//lightColor.x = sin(glfwGetTime() * 2.0f);
+		//lightColor.y = sin(glfwGetTime() * 0.7f);
+		//lightColor.z = sin(glfwGetTime() * 1.3f);
+		//
+		//glm::vec3 diffuseColor = lightColor     * glm::vec3(0.5f);
+		//glm::vec3 ambientColor = diffuseColor   * glm::vec3(0.2f);
+		//glm::vec3 specularColor = lightColor    * glm::vec3(0.5f);
 
-		ourShader.setVec3("light.ambient", ambientColor);
-		ourShader.setVec3("light.diffuse", diffuseColor);
-		ourShader.setVec3("light.specular", specularColor);
+		//ourShader.setVec3("light.ambient", ambientColor);
+		//ourShader.setVec3("light.diffuse", diffuseColor);
+		//ourShader.setVec3("light.specular", specularColor);
 
 		// render container
 		glBindVertexArray(VAO);
