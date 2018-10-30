c55bab875fdd60063f7bf2af86e98b21cdb353a6
Victor <13913968214@163.com>
2018-10-30 22:42:57 +0800

    -- Realize Desert Scene Lights.

--- prj/MixLights/src/main.cpp
+++ prj/MixLights/src/main.cpp
@@ -145,6 +145,13 @@ int main()
         glm::vec3(0.0f, 0.0f, -3.0f)
     };
 
+    glm::vec3 pointLightColors[] = {
+        glm::vec3(1.0f, 0.6f, 0.0f),
+        glm::vec3(1.0f, 0.0f, 0.0f),
+        glm::vec3(1.0f, 1.0, 0.0),
+        glm::vec3(0.2f, 0.2f, 1.0f)
+    };
+
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);;
@@ -187,48 +194,48 @@ int main()
     ourShader.setInt("material.specular", 1);
 	ourShader.setFloat("material.shininess", 32.0f);
 
-	ourShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-	ourShader.setVec3("dirLight.diffuse", 0.2f, 0.2F, 0.2f);
-	ourShader.setVec3("dirLight.specular", 0.4f, 0.4f, 0.4f);
+	ourShader.setVec3("dirLight.ambient", 0.3f, 0.24f, 0.14f);
+	ourShader.setVec3("dirLight.diffuse", 0.7f, 0.42f, 0.26f);
+	ourShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
     ourShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
 
-    ourShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
-    ourShader.setVec3("pointLights[0].diffuse", 0.2f, 0.2f, 0.2f);
-    ourShader.setVec3("pointLights[0].specular", 0.5f, 0.5f, 0.5f);
+    ourShader.setVec3("pointLights[0].ambient", pointLightColors[0].x * 0.1, pointLightColors[0].y * 0.1, pointLightColors[0].z * 0.1);
+    ourShader.setVec3("pointLights[0].diffuse", pointLightColors[0]);
+    ourShader.setVec3("pointLights[0].specular", pointLightColors[0]);
     ourShader.setFloat("pointLights[0].constant", 1.0f);
     ourShader.setFloat("pointLights[0].linear", 0.09f);
     ourShader.setFloat("pointLights[0].quadratice", 0.032f);
     ourShader.setVec3("pointLights[0].position", pointLightPositions[0]);
 
-    ourShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
-    ourShader.setVec3("pointLights[1].diffuse", 0.2f, 0.2f, 0.2f);
-    ourShader.setVec3("pointLights[1].specular", 0.5f, 0.5f, 0.5f);
+    ourShader.setVec3("pointLights[1].ambient", pointLightColors[1].x * 0.1, pointLightColors[1].y * 0.1, pointLightColors[1].z * 0.1);
+    ourShader.setVec3("pointLights[1].diffuse", pointLightColors[1]);
+    ourShader.setVec3("pointLights[1].specular", pointLightColors[1]);
     ourShader.setFloat("pointLights[1].constant", 1.0f);
     ourShader.setFloat("pointLights[1].linear", 0.09f);
     ourShader.setFloat("pointLights[1].quadratice", 0.032f);
     ourShader.setVec3("pointLights[1].position", pointLightPositions[1]);
 
-    ourShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
-    ourShader.setVec3("pointLights[2].diffuse", 0.2f, 0.2f, 0.2f);
-    ourShader.setVec3("pointLights[2].specular", 0.5f, 0.5f, 0.5f);
+    ourShader.setVec3("pointLights[2].ambient", pointLightColors[2].x * 0.1, pointLightColors[2].y * 0.1, pointLightColors[2].z * 0.1);
+    ourShader.setVec3("pointLights[2].diffuse", pointLightColors[2]);
+    ourShader.setVec3("pointLights[2].specular", pointLightColors[2]);
     ourShader.setFloat("pointLights[2].constant", 1.0f);
     ourShader.setFloat("pointLights[2].linear", 0.09f);
     ourShader.setFloat("pointLights[2].quadratice", 0.032f);
     ourShader.setVec3("pointLights[2].position", pointLightPositions[2]);
 
-    ourShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
-    ourShader.setVec3("pointLights[3].diffuse", 0.2f, 0.2f, 0.2f);
-    ourShader.setVec3("pointLights[3].specular", 0.5f, 0.5f, 0.5f);
+    ourShader.setVec3("pointLights[3].ambient", pointLightColors[3].x * 0.1, pointLightColors[3].y * 0.1, pointLightColors[3].z * 0.1);
+    ourShader.setVec3("pointLights[3].diffuse", pointLightColors[3]);
+    ourShader.setVec3("pointLights[3].specular", pointLightColors[3]);
     ourShader.setFloat("pointLights[3].constant", 1.0f);
     ourShader.setFloat("pointLights[3].linear", 0.09f);
     ourShader.setFloat("pointLights[3].quadratice", 0.032f);
     ourShader.setVec3("pointLights[3].position", pointLightPositions[3]);
 
     ourShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-    ourShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-    ourShader.setVec3("spotLight.specular", 1.0f, 0.2f, 0.3f);
+    ourShader.setVec3("spotLight.diffuse", 0.8f, 0.8f, 0.0f);
+    ourShader.setVec3("spotLight.specular", 0.8f, 0.8f, 0.0f);
     ourShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-    ourShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(17.5f)));  
+    ourShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(13.0f)));  
     ourShader.setFloat("spotLight.constant", 1.0f);
     ourShader.setFloat("spotLight.linear", 0.09f);
     ourShader.setFloat("spotLight.quadratic", 0.032f);      
@@ -258,7 +265,7 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClearColor(0.75f, 0.52f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// create transformations
